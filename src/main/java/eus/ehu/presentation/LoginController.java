c803f5ca6be61df022ded0ba413fd7be32e67a19
iratiph <ipenagaricano002@ikasle.ehu.eus>
2026-01-28 13:13:52 +0100

    Intial version

---
+++ src/main/java/eus/ehu/presentation/LoginController.java
@@ -0,0 +1,36 @@
+package eus.ehu.presentation;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML
+    private Button myButton;
+
+    @FXML
+    private Label messageLabel;
+
+    @FXML
+    private TextField messageLogin;
+
+    @FXML
+    private PasswordField messagePassword;
+
+    @FXML
+    private void handleButtonClick() {
+        if (messageLogin.getText().equals("admin") && messagePassword.getText().equals("123456")) {
+            messageLabel.setText("Correct!");
+            messageLabel.getStyleClass().setAll("btn", "btn-success");
+            messageLabel.setVisible(true);
+        } else {
+            messageLabel.setText("Try again :)"); 
+            messageLabel.getStyleClass().setAll("btn", "btn-danger");
+            messageLabel.setVisible(true);
+        }
+    }
+
+    
+}
